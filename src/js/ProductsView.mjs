5098ffa7fd8f7ebb717b1132c350d916188a86f0
Arman Abbasian <abasian.arman@gmail.com>
2022-09-07 12:29:26 +0430

    start coding ProdcutsView module

--- src/js/ProductsView.mjs
+++ src/js/ProductsView.mjs
@@ -1,3 +1,4 @@
+import SendingAPI from "./SendingAPI.mjs";
 const sendingsDataContainer=document.querySelector("#sendings_data_container");
 // import SendingAPI from "./SendingAPI.mjs";
 const productsContainer=document.querySelector(".main")
@@ -5,7 +6,8 @@ const productsContainer=document.querySelector(".main")
  class ProductsView {
   //به عنوان ورودی به کانستراکتور ی تگ پدر می دی 
     constructor() {
-    this.allSendingItems=SendingAPI.getAllSending(); 
+    this.allSendingItems=SendingAPI.getAllSending();
+    this.updateSendingList(this.allSendingItems); 
       //این جا به(روت)دسترسی نداریم به همین خاطر به عنوان ورودی پاسش میدیم
       //کلا به هر مقداری (استیتی) که در هر ماژول دسترسی نداریم به عنوان ورودی به اون کلاس پاس می دیم تا در ماژول مربوطه مقدارش رو بهش بدیم
       //make the static appearance of the sheet
@@ -14,29 +16,29 @@ const productsContainer=document.querySelector(".main")
    //تا قبل از این جا به محض ساخته شدن یک نمونه از این کلاس به طور خودکار اجرا می شود(چون داخل کانستراکتور است) 
   
  //method for creating one list item
-//     _creatListItemHTML(id, customerName, productName, numberOfSending, dateOfSending){
-//       console.log(dateOfSending)
-//       return `
-//       <div class="sendingItemBox grid grid-cols-3 gap-2 text-[9px] sm:text-xs md:text-sm shadow-xl bg-gold px-1 py-4  w-full rounded-md cursor-pointer" data-sending-id="${id}">
-//           <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
-//             <p class="flex justify-center items-center">${customerName}</p>
-//             <p class="flex justify-center items-center">${productName}</p>
-//           </div>
-//           <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
-//             <p class="flex justify-center items-center">${numberOfSending}</p>
-//             <p class="flex justify-center items-center"> ${new Date(dateOfSending).getFullYear()}/${new Date(dateOfSending).getMonth()+1}/${new Date(dateOfSending).getDate()}</p>
-//           </div>
+    _creatListItemHTML(id, customerName, productName, numberOfSending, dateOfSending){
+      console.log(dateOfSending)
+      return `
+      <div class="sendingItemBox grid grid-cols-3 gap-2 text-[9px] sm:text-xs md:text-sm shadow-xl bg-gold px-1 py-4  w-full rounded-md cursor-pointer" data-sending-id="${id}">
+          <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
+            <p class="flex justify-center items-center">${customerName}</p>
+            <p class="flex justify-center items-center">${productName}</p>
+          </div>
+          <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
+            <p class="flex justify-center items-center">${numberOfSending}</p>
+            <p class="flex justify-center items-center"> ${new Date(dateOfSending).getFullYear()}/${new Date(dateOfSending).getMonth()+1}/${new Date(dateOfSending).getDate()}</p>
+          </div>
 
-//           <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
-//               <p class="sendingItem flex justify-center items-center" data-sending-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-//                 <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-//               </svg></p>
-//               <p class="notes__list-trash flex justify-center items-center" data-trash="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="flex justify-center items-center h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-//               <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-//               </svg></p>
-//           </div>
-//       </div>`
-//     }
+          <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
+              <p class="sendingItem flex justify-center items-center" data-sending-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+              </svg></p>
+              <p class="notes__list-trash flex justify-center items-center" data-trash="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="flex justify-center items-center h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+              </svg></p>
+          </div>
+      </div>`
+    }
   
     //input for this method is all sending data
     updateSendingList(sendingsData) {
@@ -48,11 +50,10 @@ const productsContainer=document.querySelector(".main")
       //loop in all sending items and add them to noteList variable
       for (const sendingData of sendingsData) {
         const { id, customerName, productName, numberOfSending, dateOfSending } = sendingData;
-        console.log(new Date(dateOfSending).toDateString())
         //method for creating one note item
-        const html = this._creatListItemHTML(id, customerName, productName, numberOfSending, dateOfSending);
+        const oneProductRow = this._creatListItemHTML(id, customerName, productName, numberOfSending, dateOfSending);
         //add made item to the noteList variable
-        sendingsDataList += html;
+        sendingsDataList += oneProductRow;
       }
       // add notesList variable to the DOM
       sendingsDataContainer.innerHTML= sendingsDataList;
@@ -162,4 +163,5 @@ const productsContainer=document.querySelector(".main")
 
     
    }
+}
     export default new ProductsView();
--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,6 +1,7 @@
 // import App from "./App.mjs";
 import FilterView from "./FilterView.mjs";
 import FormView from "./Formview.mjs";
+import ProductsView from "./ProductsView.mjs";
 
 //get the father of all elements in html tags
 document.querySelector("body").addEventListener("DOMContentLoaded",()=>{
