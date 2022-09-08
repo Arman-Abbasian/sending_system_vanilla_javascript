5c9ed0aa428404d542592f014091d968189e725b
Arman Abbasian <abasian.arman@gmail.com>
2022-09-08 12:05:44 +0430

    work on FormView module

--- src/js/Formview.mjs
+++ src/js/Formview.mjs
@@ -1,3 +1,4 @@
+import ProductsView from "./ProductsView.mjs";
 import SendingAPI from "./SendingAPI.mjs";
 
 //get the needed elements(element that we want to add events to them and are in initeial load on website)
@@ -6,10 +7,10 @@ let customerName_input = document.querySelector("#customerName_input");
 let productName_input = document.querySelector("#productName_input");
 let numberOfSending_input = document.querySelector("#numberOfSending_input");
 let dateOfSending_input = document.querySelector("#dateOfSending_input");
-let customerName = document.querySelector(".customerName");
-let productName = document.querySelector(".productName");
-let numberOfSending = document.querySelector(".numberOfSending");
-let dateOfSending = document.querySelector(".dateOfSending");
+let customerNamePreview = document.querySelector(".customerName");
+let productNamePreview = document.querySelector(".productName");
+let numberOfSendingPreview = document.querySelector(".numberOfSending");
+let dateOfSendingPreview = document.querySelector(".dateOfSending");
 let formItemsInFormSection=document.querySelectorAll(".formm .input");
 let priviewItemsInPreviewSection=document.querySelectorAll(".previewSection .preview")
 
@@ -22,7 +23,6 @@ let priviewItemsInPreviewSection=document.querySelectorAll(".previewSection .pre
       productName_input.addEventListener("change",(e)=>this.productNameHandler(e.target.value));
       numberOfSending_input.addEventListener("change",(e)=>this.numberOfSendingHandler(e.target.value));
       dateOfSending_input.addEventListener("change",(e)=>this.dateOfSendingcustomerNameHandler(e.target.value));
-
       this.sendingData={customerName:"",productName:"",numberOfSending:0,dateOfSending:""}
 
     }
@@ -41,6 +41,7 @@ let priviewItemsInPreviewSection=document.querySelectorAll(".previewSection .pre
           element.textContent="";
        });
        SendingAPI.addOrEditSendingData(this.sendingData);
+       ProductsView.updateSendingList();
       //  this.filterSendingItem(this.filterOptions);
       //  this.checkWholeItemChanging(this.filterOptions)
       //  this.afterEventsFiltersOptions(this.root);
@@ -50,19 +51,19 @@ let priviewItemsInPreviewSection=document.querySelectorAll(".previewSection .pre
      //چون این متد تغییراتی رو در ظاهر صفحه هم ایجاد می کنه باید همین جا تعریف بشه و نمی شه در ماژول(اپ) انجام بشه
     customerNameHandler(inputValue){
         this.sendingData.customerName= inputValue;
-        customerName.textContent= inputValue;
+        customerNamePreview.textContent= inputValue;
     };
     productNameHandler(inputValue){
-      this.sendingData.customerName= inputValue;
-      productName.textContent=inputValue
+      this.sendingData.productName= inputValue;
+      productNamePreview.textContent=inputValue
   } ;
     numberOfSendingHandler(inputValue){
-    this.sendingData.customerName= inputValue;
-    numberOfSending.textContent=inputValue
+    this.sendingData.numberOfSending= inputValue;
+    numberOfSendingPreview.textContent=inputValue
   };
     dateOfSendingcustomerNameHandler(inputValue){
-  this.sendingData.customerName= inputValue;
-  dateOfSending.textContent=`${new Date(inputValue).getFullYear()}/${new Date(inputValue).getMonth()+1}/${new Date(inputValue).getDate()}`
+  this.sendingData.dateOfSending= inputValue;
+  dateOfSendingPreview.textContent=`${new Date(inputValue).getFullYear()}/${new Date(inputValue).getMonth()+1}/${new Date(inputValue).getDate()}`
   };
 
   fillSendigDataObject(){
@@ -70,8 +71,22 @@ let priviewItemsInPreviewSection=document.querySelectorAll(".previewSection .pre
         this.sendingData.productName=productName_input.value;
         this.sendingData.numberOfSending=numberOfSending_input.value;
         this.sendingData.dateOfSending=new Date (dateOfSending_input.value).toISOString();
-  }
+  };
+  fillInputs(selectedSendingItem){
+    console.log(selectedSendingItem);
+    //fill inputs
+    customerName_input.value =selectedSendingItem.customerName;
+    productName_input.value=selectedSendingItem.productName;
+    numberOfSending_input.value=selectedSendingItem.numberOfSending;
+    dateOfSending_input.value=selectedSendingItem.dateOfSending;
+    //fill prieview section
+    customerNamePreview.textContent=selectedSendingItem.customerName;
+    productNamePreview.textContent=selectedSendingItem.productName;
+    numberOfSendingPreview.textContent=selectedSendingItem.numberOfSending;
+    dateOfSendingPreview.textContent=`${new Date(selectedSendingItem.dateOfSending).getFullYear()}/${new Date(selectedSendingItem.dateOfSending).getMonth()+1}/${new Date(selectedSendingItem.dateOfSending).getDate()}`;
 
+
+  }
 
     }
     export default new FormView();
--- src/js/ProductsView.mjs
+++ src/js/ProductsView.mjs
@@ -1,25 +1,26 @@
 import SendingAPI from "./SendingAPI.mjs";
+import Formview from "./Formview.mjs";
 const sendingsDataContainer=document.querySelector("#sendings_data_container");
-// import SendingAPI from "./SendingAPI.mjs";
-const productsContainer=document.querySelector(".main")
+
 
  class ProductsView {
   //به عنوان ورودی به کانستراکتور ی تگ پدر می دی 
     constructor() {
     this.allSendingItems=SendingAPI.getAllSending();
-    this.updateSendingList(this.allSendingItems); 
+    this.filteredSendingItems=this.allSendingItems; 
+    this.updateSendingList();
       //این جا به(روت)دسترسی نداریم به همین خاطر به عنوان ورودی پاسش میدیم
       //کلا به هر مقداری (استیتی) که در هر ماژول دسترسی نداریم به عنوان ورودی به اون کلاس پاس می دیم تا در ماژول مربوطه مقدارش رو بهش بدیم
       //make the static appearance of the sheet
       }
 
    //تا قبل از این جا به محض ساخته شدن یک نمونه از این کلاس به طور خودکار اجرا می شود(چون داخل کانستراکتور است) 
-  
+ //! -------------------------------------------- 
  //method for creating one list item
     _creatListItemHTML(id, customerName, productName, numberOfSending, dateOfSending){
       console.log(dateOfSending)
       return `
-      <div class="sendingItemBox grid grid-cols-3 gap-2 text-[9px] sm:text-xs md:text-sm shadow-xl bg-gold px-1 py-4  w-full rounded-md cursor-pointer" data-sending-id="${id}">
+      <div class="sendingItemBox grid grid-cols-3 gap-2 text-[9px] sm:text-xs md:text-sm shadow-xl bg-gold px-1 py-4  w-full rounded-md cursor-pointer" data-sending="${id}">
           <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
             <p class="flex justify-center items-center">${customerName}</p>
             <p class="flex justify-center items-center">${productName}</p>
@@ -30,26 +31,29 @@ const productsContainer=document.querySelector(".main")
           </div>
 
           <div class="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-3">
-              <p class="sendingItem flex justify-center items-center" data-sending-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <p class="editItem flex justify-center items-center" data-edit="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
               </svg></p>
-              <p class="notes__list-trash flex justify-center items-center" data-trash="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="flex justify-center items-center h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <p class="deleteItem flex justify-center items-center" data-trash="${id}"><svg xmlns="http://www.w3.org/2000/svg" class="flex justify-center items-center h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
               </svg></p>
           </div>
       </div>`
     }
-  
+  //! --------------------------------------------
     //input for this method is all sending data
-    updateSendingList(sendingsData) {
+    updateSendingList() {
+      this.allSendingItems=SendingAPI.getAllSending();
+      this.filteredSendingItems=this.allSendingItems;
       //get note list items area
       //  empty all the sending container sectio in website
       sendingsDataContainer.innerHTML = "";
       // put a empty container
       let sendingsDataList = "";
       //loop in all sending items and add them to noteList variable
-      for (const sendingData of sendingsData) {
-        const { id, customerName, productName, numberOfSending, dateOfSending } = sendingData;
+      console.log(this.filteredSendingItems)
+      for (const sendingItem of this.filteredSendingItems) {
+        const { id, customerName, productName, numberOfSending, dateOfSending } = sendingItem;
         //method for creating one note item
         const oneProductRow = this._creatListItemHTML(id, customerName, productName, numberOfSending, dateOfSending);
         //add made item to the noteList variable
@@ -57,111 +61,110 @@ const productsContainer=document.querySelector(".main")
       }
       // add notesList variable to the DOM
       sendingsDataContainer.innerHTML= sendingsDataList;
-
-
-      sendingsDataContainer.querySelectorAll(".sendingItemBox").forEach((sendingDataBox) => {
+      this.addEventToProductItems()
+    };
+//! --------------------------------------------
+    // this method add evenets to product items 
+    addEventToProductItems(){
+      //1- add click event to <div class='sendingItemBox'...>
+      document.querySelectorAll(".sendingItemBox").forEach((sendingDataBox) => {
         sendingDataBox.addEventListener("click", () =>{
           //input for this method is id
-          this.onSelectData(sendingDataBox.dataset.sendingId);
-          this.filterSendingItem(this.filterOptions);
+          this.onSelectSendingData(sendingDataBox.dataset.sending);
+          //this.filterSendingItem(this.filterOptions);
         }
         );
       });
 
-//       //add click event for select to the notesList(این رو مجبوریم این جا اضافه کنیم چون این جا به دام ما اضافه شده)
-//       sendingsDataContainer.querySelectorAll(".sendingItem").forEach((sendingData) => {
-//         sendingData.addEventListener("click", (e) =>{
-//           //input for this method is id
-//           e.stopPropagation();
-//           this.onEditSendingData(sendingData.dataset.sendingId);
-//           this.filterSendingItem(this.filterOptions);
-//           this.initailsFiltersOptions(this.root,allSending)
-//         }
-//         );
-//       });
-//       //add click event for delete to the notesList(این رو مجبوریم این جا اضافه کنیم چون این جا به دام ما اضافه شده)
-//       sendingsDataContainer
-//         .querySelectorAll(".notes__list-trash")
-//         .forEach((noteItem) => {
-//           noteItem.addEventListener("click", (e) => {
-//             //چون پدرش هم دارای کلیک هست برای جلوگیری از اختلاط رویداد ها از این کد استفاده می کنیم
-//             e.preventDefault();
-//             e.stopPropagation();
-//             this.onDeleteSendingData(noteItem.dataset.trash);
-//             console.log(this.filterOptions);
-//             this.checkWholeItemChanging(this.filterOptions);
-//             console.log(this.filterOptions);
-//             this.filterSendingItem(this.filterOptions);
-//             console.log(this.filterOptions);
-//             this.afterEventsFiltersOptions(this.root,this.filterOptions);
-//             console.log(this.filterOptions);
+      //2- add click event to <p class="sendingItem" ...> for edit product item(این رو مجبوریم این جا اضافه کنیم چون این جا به دام ما اضافه شده)
+      sendingsDataContainer.querySelectorAll(".editItem").forEach((sendingData) => {
+        sendingData.addEventListener("click", (e) =>{
+          e.stopPropagation();
+          this.onEditSendingData(sendingData.dataset.edit);
+          //this.filterSendingItem(this.filterOptions);
+          //this.initailsFiltersOptions(this.root,allSending)
+        }
+        );
+      });
+      //3- add click event to delete  the product item(این رو مجبوریم این جا اضافه کنیم چون این جا به دام ما اضافه شده)
+      sendingsDataContainer.querySelectorAll(".deleteItem").forEach((noteItem) => {
+        noteItem.addEventListener("click", (e) => {
+            //چون پدرش هم دارای کلیک هست برای جلوگیری از اختلاط رویداد ها از این کد استفاده می کنیم
+            e.preventDefault();
+            e.stopPropagation();
+            this.onDeleteSendingData(noteItem.dataset.trash);
+            console.log(this.filterOptions);
+            // this.checkWholeItemChanging(this.filterOptions);
+            // console.log(this.filterOptions);
+            // this.filterSendingItem(this.filterOptions);
+            // console.log(this.filterOptions);
+            // this.afterEventsFiltersOptions(this.root,this.filterOptions);
+            // console.log(this.filterOptions);
             
-//           });
-//         });
-//     };
+          });
+        });
+      }
+   //! --------------------------------------------
+    //for changing  the value of selected note item
+    //the input for the mothod is data of selected item
+      updateActiveSendingItem(sendingItem) {
+        console.log(sendingItem.id)
+        console.log(document.querySelector(`.sendingItemBox[data-sending="${sendingItem.id}"]`))
+      //  remove 'notes__list-item--selected' from all note items
+      document.querySelectorAll(".sendingItemBox").forEach((item) => {
+        item.classList.remove("bg-lime-400");
+      });
+       //add 'notes__list-item--selected' to the selected note item
+       console.log(document.querySelector(`.sendingItemBox[data-sending="${sendingItem.id}"]`))
+      document.querySelector(`.sendingItemBox[data-sending="${sendingItem.id}"]`).classList.add("bg-lime-400");
+    }
+//! --------------------------------------------
+    showFixedPart(selectedBoxId){
+        const box=`<div>
+        <p class="p-10 bg-lightGreen rounded-md text-base relative">you sent <span class="text-blue-800">${selectedBoxId.numberOfSending}</span> numbers of <span class="text-blue-800">${selectedBoxId.productName}</span> for <span class="text-blue-800">${selectedBoxId.customerName}</span> in date <span class="text-blue-800">${new Date (selectedBoxId.dateOfSending).getFullYear()}/${new Date (selectedBoxId.dateOfSending).getMonth()+1}/${new Date (selectedBoxId.dateOfSending).getDate()}<span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="closeBox w-6 h-6 absolute right-0 top-0 text-red-600 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </p>
+      </div>`
+      const boxx=document.querySelector("#box");
+      boxx.classList.remove("hidden")
+      boxx.classList.add("flex")
+      boxx.innerHTML=box;
+      let closeBoxx=document.querySelector(".closeBox");
+      closeBoxx.addEventListener("click",()=>this.closeBox())  
+    };
 
+    //! --------------------------------------------
+      //input for this method is the id for selected sending item
+      onSelectSendingData(sendingItemId){
+        console.log(sendingItemId)
+        const selectedSendingItem = this.allSendingItems.find((n) => n.id == sendingItemId);
+        console.log(selectedSendingItem)
+        this.showFixedPart(selectedSendingItem)
+        //input for this method is data for clicked sending item
+      };
 
-//     //for changing  the value of selected note item
-//     //the input for the mothod is data of selected item
-//       updateActiveSendingItem(sendingItem) {
-//       //  remove 'notes__list-item--selected' from all note items
-//       this.root.querySelectorAll(".sendingItem").forEach((item) => {
-//         console.log(item.parentElement.parentElement)
-//         item.parentElement.parentElement.classList.remove("bg-lime-400");
-//       });
-//        //add 'notes__list-item--selected' to the selected note item
-//       this.root.querySelector(`.sendingItem[data-sending-id="${sendingItem.id}"]`)
-//       .parentElement.parentElement
-//        .classList.add("bg-lime-400");
-//     }
+//! --------------------------------------------
 
-//     showFixedPart(selectedBoxId){
-//         console.log(selectedBoxId);
-//         const box=`<div>
-//         <p class="p-10 bg-lightGreen rounded-md text-base relative">you sent <span class="text-blue-800">${selectedBoxId.numberOfSending}</span> numbers of <span class="text-blue-800">${selectedBoxId.productName}</span> for <span class="text-blue-800">${selectedBoxId.customerName}</span> in date <span class="text-blue-800">${new Date (selectedBoxId.dateOfSending).getFullYear()}/${new Date (selectedBoxId.dateOfSending).getMonth()+1}/${new Date (selectedBoxId.dateOfSending).getDate()}<span>
-//           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="closeBox w-6 h-6 absolute right-0 top-0 text-red-600 cursor-pointer">
-//             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-//           </svg>
-//         </p>
-//       </div>`
-//       console.log(box)
-//       const boxx=this.root.querySelector("#box");
-//       boxx.classList.remove("hidden")
-//       boxx.classList.add("flex")
-//       boxx.innerHTML=box;
-//       let closeBoxx=this.root.querySelector(".closeBox");
-//       closeBoxx.addEventListener("click",()=>this.closeBox())  
-//     };
+    onEditSendingData(sendingItemId) {
+        const selectedSendingItem = this.allSendingItems.find((n) => n.id == sendingItemId);
+         Formview.fillInputs(selectedSendingItem)
+        //input for this method is data for clicked sending item
+        this.updateActiveSendingItem(selectedSendingItem);
+      };
 
+      //! --------------------------------------------
+      onDeleteSendingData(sendingItemId){
+        SendingAPI.deleteSendingItem(sendingItemId);
+        this.updateSendingList();
+      };
 
+      //! --------------------------------------------
 
-//     onEditSendingData(sendingItemId) {
-//         console.log(sendingItemId)
-//         const selectedSendingItem = this.allSendingData.find((n) => n.id == sendingItemId);
-//         console.log(selectedSendingItem)
-//         //input for this method is data for clicked sending item
-//         this._setActiveSendingItem(selectedSendingItem);
-//       };
-//       //input for this method is the id for selected sending item
-//       onSelectData(sendingItemId){
-//         console.log(sendingItemId)
-//         const selectedSendingItem = this.allSendingData.find((n) => n.id == sendingItemId);
-//         console.log(selectedSendingItem)
-//         PageView.showFixedPart(selectedSendingItem)
-//         //input for this method is data for clicked sending item
-//       };
-//       onDeleteSendingData(sendingItemId){
-//         SendingAPI.deleteSendingItem(sendingItemId);
-//         this._refreshSendingItems();
-//       };
-
-
-//       closeBox(){
-//         PageView.root.querySelector("#box").classList.add("hidden");
-//         PageView.root.querySelector("#box").classList.remove("flex");
-//       };
-
-    
+      closeBox(){
+        document.querySelector("#box").classList.add("hidden");
+        document.querySelector("#box").classList.remove("flex");
+      }; 
    }
-}
     export default new ProductsView();
